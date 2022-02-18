7322760c3a098689fe2a3c3e869623f1c88faa0b
robustjumprope <91493312+robustjumprope@users.noreply.github.com>
2022-02-17 22:38:56 -0500

    set up express server for production

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connectDb = require('./config/db')
 const dotenv = require('dotenv').config()
+const path = require('path')
 
 //Set up app connections
 const app = express()
@@ -13,6 +14,21 @@ app.use('/api/notes', require('./routes/noteRoutes'))
 
 //Set up user routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+// Show frontend pages
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+    )
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('please set environment to production')
+  })
+}
 
 //Start server
 app.listen(process.env.PORT || 5000, () => {
